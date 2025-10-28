b2a2469b22c8a5ba9e54d35e28dacb9f765bcc74
ノエルの犬 <gucooing@alsl.xyz>
2025-10-29 01:44:10 +0800

    添加港澳台支持

--- lib/http/init.dart
+++ lib/http/init.dart
@@ -4,7 +4,9 @@ import 'dart:io';
 
 import 'package:PiliPlus/http/api.dart';
 import 'package:PiliPlus/http/constants.dart';
+import 'package:PiliPlus/http/hk_api_retry_interceptor.dart';
 import 'package:PiliPlus/http/loading_state.dart';
+import 'package:PiliPlus/http/hk_api_retry_interceptor.dart';
 import 'package:PiliPlus/http/retry_interceptor.dart';
 import 'package:PiliPlus/http/user.dart';
 import 'package:PiliPlus/utils/accounts.dart';
@@ -231,6 +233,9 @@ class Request {
         RetryInterceptor(dio, Pref.retryCount, Pref.retryDelay),
       );
     }
+
+    // 港澳台支持
+    dio.interceptors.add(HkApiRetryInterceptor());
 
     // 日志拦截器 输出请求、响应内容
     if (kDebugMode) {
--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -66,6 +66,7 @@ abstract final class SettingBoxKey {
       applyFilterToRelatedVideos = 'applyFilterToRelatedVideos',
       autoUpdate = 'autoUpdate',
       maxCacheSize = 'maxCacheSize',
+      apiHKUrl = 'apiHKUrl',
       defaultShowComment = 'defaultShowComment',
       replySortType = 'replySortType',
       reply2SortType = 'reply2SortType',
