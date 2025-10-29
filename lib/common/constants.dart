fae3c296fbfe2eeed43187e4a2b060458ee3944b
ノエルの犬 <gucooing@alsl.xyz>
2025-10-29 16:22:24 +0800

    修改包名共存

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -27,7 +27,8 @@ abstract final class StyleString {
 
 abstract final class Constants {
   static const appName = 'PiliPlus';
-  static const sourceCodeUrl = 'https://github.com/qinshah/PiliPlus';
+  static const sourceCodeUrl = 'https://github.com/gucooing/PiliPlus';
+  static const upstreamCodeUrl = 'https://github.com/bggRGjQaUbCoE/PiliPlus';
 
   // 27eb53fc9058f8c3  移动端 Android
   // 4409e2ce8ffd12b8  HD版
