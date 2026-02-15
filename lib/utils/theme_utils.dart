4d87c07b975daf59b65f90e08363012ae80e0ef6
систем <oscp.asia@petalmail.com>
2026-02-15 19:20:54 +0800

    feat: Android predictive back
    实现了安卓预测性返回 (过渡动画为"native"时)
    
    ---------
    
    Co-authored-by: Chenx Dust <chenx_dust@outlook.com>

--- lib/utils/theme_utils.dart
+++ lib/utils/theme_utils.dart
@@ -153,7 +153,7 @@ abstract final class ThemeUtils {
       ),
       pageTransitionsTheme: const PageTransitionsTheme(
         builders: {
-          TargetPlatform.android: ZoomPageTransitionsBuilder(),
+          TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
         },
       ),
     );
