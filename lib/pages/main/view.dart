fc3241210a837ebe6e3630bfbb5b812b88e894af
систем <oscp.asia@petalmail.com>
2026-02-15 19:20:54 +0800

    feat: Android predictive back
    实现了安卓预测性返回 (过渡动画为"native"时)
    
    ---------
    
    Co-authored-by: Chenx Dust <chenx_dust@outlook.com>

--- lib/pages/main/view.dart
+++ lib/pages/main/view.dart
@@ -68,6 +68,10 @@ class _MainAppState extends PopScopeState<MainApp>
       // FlutterSmartDialog throws
       PiliScheme.init();
     }
+    canPopNotifier.value = _canPop();
+    ever(_mainController.selectedIndex, (int index) {
+      canPopNotifier.value = _canPop();
+    });
   }
 
   @override
@@ -258,6 +262,11 @@ class _MainAppState extends PopScopeState<MainApp>
     await trayManager.setContextMenu(trayMenu);
   }
 
+  bool _canPop() {
+    return _mainController.directExitOnBack ||
+        _mainController.selectedIndex.value == 0;
+  }
+
   void _onBack() {
     if (PlatformUtils.isDesktop) {
       onWindowClose();
@@ -268,18 +277,14 @@ class _MainAppState extends PopScopeState<MainApp>
 
   @override
   void onPopInvokedWithResult(bool didPop, Object? result) {
-    if (_mainController.directExitOnBack) {
+    if (didPop) {
       _onBack();
     } else {
-      if (_mainController.selectedIndex.value != 0) {
-        _mainController
-          ..setIndex(0)
-          ..barOffset?.value = 0.0
-          ..showBottomBar?.value = true
-          ..setSearchBar();
-      } else {
-        _onBack();
-      }
+      _mainController
+        ..setIndex(0)
+        ..barOffset?.value = 0.0
+        ..showBottomBar?.value = true
+        ..setSearchBar();
     }
   }
 
