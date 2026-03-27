34cffed13f1fa24993870f066ce19e9ec7afeceb
qshh <101165772+qinshah@users.noreply.github.com>
2026-03-27 09:13:34 +0800

    feat(image_utils): 添加鸿蒙系统图片保存权限处理
    - 鸿蒙图片保存权限由插件后续请求，因此直接返回true

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -76,6 +76,7 @@ abstract final class ImageUtils {
   }
 
   static Future<bool> checkPermissionDependOnSdkInt() async {
+    if (PlatformUtils.isHarmony) return true; // 鸿蒙图片保存权限由插件后续请求
     if (Platform.isAndroid) {
       if (await Utils.sdkInt < 29) {
         return requestPer();
