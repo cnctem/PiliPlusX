ab8069a932cd296cae5fc5c337b129b2f85bf686
систем <oscp.asia@petalmail.com>
2026-04-10 09:06:54 +0800

    feat: enablePredictiveBack for Android not default)
    Android 上可以通过"预测性返回"开关来控制是否使用原生动画，关闭后就会使用"页面过渡动画"中设置的动画效果。

--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -140,6 +140,7 @@ abstract final class SettingBoxKey {
       showClipboardSearch = 'showClipboardSearch',
       showPgcTimeline = 'showPgcTimeline',
       pageTransition = 'pageTransition',
+      enablePredictiveBack = 'enablePredictiveBack',
       optTabletNav = 'optTabletNav',
       banWordForDyn = 'banWordForDyn',
       enableLog = 'enableLog',
