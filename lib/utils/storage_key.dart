b4d95b5f4595b9998bb42a3f93bac3b6a4de1a7c
систем <oscp.asia@petalmail.com>
2026-07-08 16:10:19 +0800

    feat: 首页搜索框新增剪贴板一键粘贴搜索
    点击搜索框旁的粘贴按钮可直接跳转搜索剪贴板内容，支持开关显示按钮，
    并可设置剪贴板搜索是否计入搜索历史。

--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -132,6 +132,8 @@ abstract final class SettingBoxKey {
       appFontWeight = 'appFontWeight',
       fastForBackwardDuration = 'fastForBackwardDuration',
       recordSearchHistory = 'recordSearchHistory',
+      clipboardSearchIncognito = 'clipboardSearchIncognito',
+      showClipboardSearch = 'showClipboardSearch',
       showPgcTimeline = 'showPgcTimeline',
       pageTransition = 'pageTransition',
       optTabletNav = 'optTabletNav',
