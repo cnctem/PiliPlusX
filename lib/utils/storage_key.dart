6026a12547110e9c2eaa6772d859514f15e60444
систем <oscp.asia@petalmail.com>
2026-07-08 17:33:30 +0800

    feat: 长按视频卡片可直接加入稍后再看
    增加设置项：长按展示视频卡片替换为加入稍后再看

--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -65,6 +65,7 @@ abstract final class SettingBoxKey {
       showDynInteraction = 'showDynInteraction',
       enableHotKey = 'enableHotKey',
       defaultShowWatchLater = 'defaultShowWatchLater',
+      defaultAddWatchLater = 'defaultAddWatchLater',
       enableSearchRcmd = 'enableSearchRcmd',
       searchTypeSort = 'searchTypeSort',
       enableQuickFav = 'enableQuickFav',
