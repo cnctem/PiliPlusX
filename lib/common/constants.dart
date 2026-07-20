8e7c6cd0aa7f4ad2fdf05411961a5737dd410c83
cnoim <cnoim@qq.com>
2026-07-20 19:20:11 +0800

    更新仓库地址

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -27,7 +27,7 @@ abstract final class StyleString {
 
 abstract final class Constants {
   static const appName = 'PiliPlus';
-  static const sourceCodeUrl = 'https://github.com/qinshah/PiliPlus';
+  static const sourceCodeUrl = 'https://github.com/dev4harmony/PiliPlus';
 
   // 27eb53fc9058f8c3  移动端 Android
   // 4409e2ce8ffd12b8  HD版
