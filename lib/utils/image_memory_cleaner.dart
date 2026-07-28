54693cc8c41353dca7242ac073a0862db3981984
cnoim <cnoim@qq.com>
2026-07-28 22:49:55 +0800

    应用退后台清理图片缓存，降后台内存

---
+++ lib/utils/image_memory_cleaner.dart
@@ -0,0 +1,32 @@
+  import 'package:flutter/widgets.dart';
+
+/// 监听应用生命周期，进入后台后清理 Flutter 内存图片缓存
+class ImageMemoryCleaner with WidgetsBindingObserver {
+  ImageMemoryCleaner._internal();
+
+  static final ImageMemoryCleaner instance = ImageMemoryCleaner._internal();
+
+  void register() {
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  void unregister() {
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  void clearImageCache() {
+    try {
+      PaintingBinding.instance.imageCache.clear();
+    } catch (_) {
+      // 忽略清理过程中的异常
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused || state == AppLifecycleState.detached) {
+        debugPrint('ImageMemoryCleaner: clear image cache in background');
+        clearImageCache();
+    }
+  }
+}
